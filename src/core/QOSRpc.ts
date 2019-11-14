2c241be133978724a871b9a999c1d67ab11a389f
wangkuanzzu <wangkuan.zzu@qq.com>
2019-11-14 14:27:20 +0800

    modify

--- src/core/QOSRpc.ts
+++ src/core/QOSRpc.ts
@@ -19,11 +19,13 @@ export class QOSRpc {
   }
   public config: { readonly baseUrl: string; };
   public key: SecretKey;
+  public account: Account
   private _tx!: Tx;
 
   constructor(config: { readonly baseUrl: string; }) {
     this.config = config;
     this.key = new SecretKey();
+    this.account = new Account(this);
   }
   public post(url:string, data:IBaseInput){
     return this.request.post(url, data);
